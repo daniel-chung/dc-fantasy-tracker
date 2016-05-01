a7cfbabd8ed9240cf300153ea64d497d9824d3a4
daniel-chung <chung.dan.s@gmail.com>
2016-05-01 12:18:20 -0400

    Added support for display button toggling

--- public/ng-app/pages/pgplayer/pgplayer-controller.js
+++ public/ng-app/pages/pgplayer/pgplayer-controller.js
@@ -4,9 +4,26 @@
 var FbsPgPlayer = angular.module('fbs.pgplayer', []);
 
 FbsPgPlayer.controller('fbs.pgplayer.pgplayerCtrl', function(
-    $scope, $stateParams, pgPlayerData) {
+    $scope, $stateParams, pgPlayerData, $state) {
   $scope.playerId = $stateParams.pid;
   $scope.playerStats = pgPlayerData;
+
+  // Populate the correct button colors on load
+  $scope.fbsBtnTable = 
+    $state.current.name == 'pgplayer.table' ? '' : 'fbs-btn-unselected';
+  $scope.fbsBtnChart = 
+    $state.current.name == 'pgplayer.chart' ? '' : 'fbs-btn-unselected';
+
+  $scope.changeClass = function(className) {
+    if (className == 'fbsBtnTable') {
+      $scope.fbsBtnTable = '';
+      $scope.fbsBtnChart = 'fbs-btn-unselected';
+    }
+    else {
+      $scope.fbsBtnTable = 'fbs-btn-unselected';
+      $scope.fbsBtnChart = '';
+    }
+  };
 });
 
 
